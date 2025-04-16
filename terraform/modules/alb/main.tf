65ba61d4e0d3c69475097c86277ef8efbb6f0ad9
nirajku103 <nirajnetjob.hojai@gmail.com>
2025-04-16 22:42:00 +0000

    adding targets

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -42,7 +42,7 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_lb_target_group" "openproject" {
   name     = "openproject-tg"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "instance"

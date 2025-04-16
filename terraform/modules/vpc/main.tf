199a9ef264eff1a6e674859fc723666c434dca10
nirajku103 <nirajnetjob.hojai@gmail.com>
2025-04-16 22:20:23 +0000

    adding targets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -9,7 +9,7 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count = 1
+  count = 2
   vpc_id = aws_vpc.main.id
   cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
